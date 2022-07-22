527271339ef7ecd185b9b451146aac15cd2114ae
jonasmachados <jonasmachado.ti@gmail.com>
2022-07-22 18:21:25 -0300

    Project created

---
+++ src/main/java/com/bearmanagement/BearManagementApplication.java
@@ -0,0 +1,13 @@
+package com.bearmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BearManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BearManagementApplication.class, args);
+	}
+
+}
